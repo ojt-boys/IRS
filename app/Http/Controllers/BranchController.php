d5100ab2ec93207131ee8af2771aa8c0a899d860
Daerko <johnuelpructo7@gmail.com>
2025-03-21 16:29:36 +0800

    sit-routes-rename-fix
    sit-routes-rename-fix

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -6,34 +6,18 @@ use Inertia\Inertia;
 
 class BranchController extends Controller
 {
-    // Show branch dashboard
-    public function branchDashboard()
-    {
-        // Sample data for dashboard
-        return Inertia::render('Branch/branchDashboard');
-    }
 
     // Show form to add new shoes to the branch
-    public function branchAddshoes()
+    public function Addshoes()
     {
-        return Inertia::render('Branch/branchAddshoes');
+        return Inertia::render('Branch/Addshoes');
     }
 
     // Show list of shipped shoes for the branch
-    public function branchShippedshoes()
+    public function Shippedshoes()
     {
-        return Inertia::render('Branch/branchShippedshoes');
+        return Inertia::render('Branch/Shippedshoes');
     }
 
-    // Show list of items for return in the branch
-    public function branchReturned()
-    {
-        return Inertia::render('Branch/branchReturned');
-    }
-
-    // Show completed branch tasks
-    public function branchCompleted()
-    {
-        return Inertia::render('Branch/branchCompleted');
-    }
+   
 }
